33652d1c9f51149ff899a420b32757285160e05a
NarukamiYaeAsuna <nguyengiangsun@gmail.com>
2026-01-26 15:29:25 +0700

    WHitebox,BLACKbox

--- unit-test/src/test/java/StudentAnalyzerTest.java
+++ unit-test/src/test/java/StudentAnalyzerTest.java
@@ -1,8 +1,9 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class StudentAnalyzerTest {
 
